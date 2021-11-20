e9a05253830def9513327b8c446ab40502b8b07b
Rashmi Sharma <rashmisharma0264@gmail.com>
2021-11-20 17:44:01 +0000

    simple script to list files and folders within /etc and /tmp

---
+++ listFolders.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# author = rashmi sharma
+
+
+
+
+echo "listing folders and files available in /etc directory"
+ls -l /etc/
+
+echo "listing folders and files available in /tmp directory"
+ls -l /tmp/
+
