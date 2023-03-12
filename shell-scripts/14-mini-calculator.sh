77df13b044225901928a3e0d9fed5f11ae807704
Rashmi Sharma <rashmisharma0264@gmail.com>
2023-03-12 18:59:17 +0000

    Calculator for Addition, Substraction, Multiplication and Divition of Two numbers

---
+++ shell-scripts/14-mini-calculator.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "enter no.1"
+read n1
+echo "enter no.2"
+read n2
+
+echo "addition"
+sum=`expr $n1 + $n2`
+echo "sum = $sum"
+
+echo "substraction"
+sub=`expr $n1 - $n2`
+echo "sub = $sub" 
+
+echo "multiplication"
+mul=`expr $n1 \* $n2`
+echo "mul = $mul"
+
+echo "division"
+div=`expr $n1 / $n2`
+echo "div = $div"
